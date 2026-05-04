45d76ba7a03b75ebc1e8e1dbeb2e72e84d0ccf3b
Arpit Poudel <arpitpoudel97@gmail.com>
2026-05-05 00:17:43 +0545

    it was a syntax error

--- src/inputUser.java
+++ src/inputUser.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 public class inputUser {
     public static void main(String[] args){
--- src/randomClass.java
+++ src/randomClass.java
@@ -1,25 +1,27 @@
 import java.util.Random;
 public class randomClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //here we are learning to operate Random numbers.
 
         Random random = new Random();
 
         int num1;
         double num2;
-        Boolean ishead;
+        boolean ishead;
 
-        num1 = random.nextInt(1,7);//second number is exclusive.
+        num1 = random.nextInt(1, 7);//second number is exclusive.
         System.out.println(num1);
 
         num2 = random.nextDouble();
         System.out.println(num2);
 
         ishead = random.nextBoolean();
-        System.out.println(ishead);
 
+        if (ishead)
+            System.out.println("it is head");
 
-
-
+        else {
+            System.out.println("it is tail.");
+        }
     }
 }
