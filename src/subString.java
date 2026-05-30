4b16ae83db9697e74f68e5a75b858e921716f17d
Arpit Poudel <arpitpoudel97@gmail.com>
2026-05-30 08:00:53 +0545

    init commit

--- src/subString.java
+++ src/subString.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
 public class subString {
     public static void main(String[] args){
         //.subString() = A method used to extract portion of a string
         // string.subtring(start,end)
-         String email = "arpitpoudel@gmail.com";
-         String username = email.substring(0,11);
-        System.out.println(username);
-         String domain = email.substring(12,21);
-        System.out.println(domain);
+
+        Scanner input = new Scanner(System.in);
+
+        String email,username,domain;
+        System.out.print("Enter email: ");
+        email = input.nextLine();
+
+        if(email.contains("@")) {
+            username = email.substring(0, email.indexOf('@'));
+            domain = email.substring(email.indexOf('@') + 1);
+
+            System.out.println("The username is " + username + ".");
+            System.out.println("The domain of the email is " + domain + ".");
+        }
+        else {
+            System.out.println("The email is not valid.");
+        }
 
     }
 }
