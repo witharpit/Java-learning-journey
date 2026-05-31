de7f404a275cda01fd656c90bd14c652c5cf70d9
Arpit Poudel <arpitpoudel97@gmail.com>
2026-05-31 21:28:13 +0545

    learnt about switches

---
+++ src/enhanceSwitches.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class enhanceSwitches {
+    public static void main(String[] args){
+        //enhanced switches = replacement to many else if statement
+        //  (Java14)
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("enter a day: ");
+        String day = input.nextLine();
+
+//        switch(day){//examine the value or variable inside a paranthesis.
+//            case "monday" -> System.out.println("it is a week day");
+//            //arrow means do something
+//            case "tuesday" -> System.out.println("it is a week day");
+//            case "wednesday" -> System.out.println("it is a week day");
+//            case "thrusday" -> System.out.println("it is a week day");
+//            case "friday" -> System.out.println("it is a week day");
+//            case "saturday" -> System.out.println("it is a weekend");
+//            case "sunday" -> System.out.println("it is a weekend");
+//
+//            default -> System.out.println("it is not a day");
+
+        //consolidate
+            switch(day){//examine the value or variable inside a paranthesis.
+
+                case "monday","tuesday","wednesday","thrusday","friday" ->
+                        System.out.println("it is the week day");
+
+                case "saturday","sunday" ->
+                        System.out.println("it is the weekend");
+
+                default -> System.out.println("it is not a day");
+
+
+        }
+
+
+    }
+}
