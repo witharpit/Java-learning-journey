ed1c6510e8a642a58609de7433b217c460713757
Arpit Poudel <arpitpoudel97@gmail.com>
2026-06-04 08:59:33 +0545

    learnt about switches

--- src/temperatureConverter.java
+++ src/temperatureConverter.java
@@ -11,12 +11,15 @@ public class temperatureConverter {
         System.out.print("Enter the temperature: ");
         temp = input.nextDouble();
 
+        input.nextLine();
+
         System.out.print("Convert to celsius to farenhiet (C or F):  ");
         unit = input.next().toUpperCase();
 
-        newTemp = (unit.equals("C")) ? (temp-32)*5/9 : (temp*5/9)+32;
+        newTemp = (unit.equals("C")) ? (temp-32)*5.0/9.0 : (temp*5.0/9.0)+32;
         System.out.printf("The converted temperature is %.1f°%s.", newTemp,unit);
 
+        input.close();
 
     }
 }
